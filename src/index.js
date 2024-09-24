59654cf3c2e1481ea639cc9926bcba59ee9a5855
DanielMjosRoli <daniel.mrfush@gmail.com>
2024-09-24 13:58:44 +0200

    Done with core and first part of extensions

--- src/index.js
+++ src/index.js
@@ -4,3 +4,42 @@ console.log(data);
 //You can start simple and just render a single 
 //pokemon card from the first element
 console.log(data[0]);
+function renderCards(){
+    var target = document.getElementById("cards")
+    target.innerHTML = ''
+    data.forEach((element) => {
+        target.innerHTML += `
+        <li class="card">
+            <h2 class="card--title">${element.name}</h2>
+            <img
+                width="256"
+                class="card--img"
+                src="${element.sprites.other["official-artwork"].front_default}"
+            />
+            <ul class="card--text">
+                ${getStats(element.stats)}
+            </ul> 
+            <ul class="card--text">
+                <p>Game appearances</p>
+                ${getGames(element.game_indices)}
+            </ul>
+        </li>`
+    })
+    console.log(target.innerHTML)
+}
+function getStats(array){
+    let returnString = ''
+    array.forEach((stat) => {
+        returnString += `<li>${stat.stat.name}: ${stat.base_stat}</li>`
+    })
+    return returnString
+}
+function getGames(array){
+    let returnString = ''
+    array.forEach((stat) => {
+        returnString += `<li>${stat.version.name}: Pokedex Nr ${stat.game_index}</li>`
+    })
+    return returnString
+}
+
+renderCards()
